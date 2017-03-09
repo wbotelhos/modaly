f65db0d3cbae94e16eb778645b8b04f496ca79e1
Washington Botelho <wbotelhos@gmail.com>
2017-03-09 16:13:52 -0300

    lint

--- spec/lib/jquery.factory.js
+++ spec/lib/jquery.factory.js
@@ -1,14 +1,18 @@
 clean = true;
 
 function context(description, spec) {
+  'use strict';
+
   describe(description, spec);
 };
 
 var Factory = {
   init: function() {
-  }, _checks: function(tag, data) {
+  },
+
+  _checks: function(tag, data) {
     var type   = $(tag)[0].type,
-        single = tag.indexOf(' />') !== -1;
+      single = tag.indexOf(' />') !== -1;
 
     if (data.checked) {
       if (type === 'checkbox') {
@@ -27,22 +31,28 @@ var Factory = {
     }
 
     return tag;
-  }, _data: function(options) {
+  },
+
+  _data: function(options) {
     options = options || {};
 
     var times      = Factory._getTimes(options),
-        html       = Factory._getHtml(options),
-        checked    = Factory._getChecked(options),
-        selected   = Factory._getSelected(options),
-        opt        = Factory._normalize(options),
-        attributes = Factory._parameterize(opt);
+      html       = Factory._getHtml(options),
+      checked    = Factory._getChecked(options),
+      selected   = Factory._getSelected(options),
+      opt        = Factory._normalize(options),
+      attributes = Factory._parameterize(opt);
 
     return { attributes: attributes, html: html, checked: checked, selected: selected, times: times };
-  }, _getChecked: function(options) {
+  },
+
+  _getChecked: function(options) {
     return options.checked || false;
-  }, _getHtml: function(options) {
+  },
+
+  _getHtml: function(options) {
     var html    = options.html || '',
-        content = '';
+      content = '';
 
     if (html instanceof Array) {
       for (var i = 0; i < html.length; i++) {
@@ -53,18 +63,26 @@ var Factory = {
     }
 
     return content;
-  }, _getSelected: function(options) {
+  },
+
+  _getSelected: function(options) {
     return options.selected || false;
-  }, _getTimes: function(options) {
+  },
+
+  _getTimes: function(options) {
     return options.times || 1;
-  }, _normalize: function(options) {
+  },
+
+  _normalize: function(options) {
     delete options.times;
     delete options.html;
     delete options.selected;
     delete options.checked;
 
     return options;
-  }, _parameterize: function(options) {
+  },
+
+  _parameterize: function(options) {
     var content = '';
 
     for (var option in options) {
@@ -72,7 +90,9 @@ var Factory = {
     }
 
     return content.replace(/\s$/, '');
-  }, _repeat: function(tag, data) {
+  },
+
+  _repeat: function(tag, data) {
     var html = '';
 
     for (var i = 0; i < data.times; i++) {
@@ -80,17 +100,25 @@ var Factory = {
     }
 
     return html;
-  }, _verify: function(options) {
+  },
+
+  _verify: function(options) {
     if (options && options.type) {
       $.error('You cannot set the "type" using an alias!');
     }
-  }, append: function(html) {
+  },
+
+  append: function(html) {
     return $(html).appendTo('.factory');
-  }, checkbox: function(options) {
+  },
+
+  checkbox: function(options) {
     Factory._verify(options);
 
     return Factory.input(options, 'checkbox');
-  }, clear: function(args) {
+  },
+
+  clear: function(args) {
     if (clean) {
       $('.factory').empty();
 
@@ -102,22 +130,34 @@ var Factory = {
         $(args).remove();
       }
     }
-  }, double: function(options, name) {
+  },
+
+  double: function(options, name) {
     var data = Factory._data(options),
-        tag  = '<' + name + ' ' + data.attributes + '>' + data.html + '</' + name + '>';
+      tag  = '<' + name + ' ' + data.attributes + '>' + data.html + '</' + name + '>';
 
     return Factory._repeat(tag.replace(' >', '>'), data);
-  }, fieldset: function(options) {
+  },
+
+  fieldset: function(options) {
     return Factory.double(options, 'fieldset');
-  }, form: function(options) {
+  },
+
+  form: function(options) {
     return Factory.double(options, 'form');
-  }, hidden: function(options) {
+  },
+
+  hidden: function(options) {
     Factory._verify(options);
 
     return Factory.input(options, 'hidden');
-  }, html: function(html) {
+  },
+
+  html: function(html) {
     $('.factory').html(html);
-  }, input: function(options, type) {
+  },
+
+  input: function(options, type) {
     options = options || {};
 
     if (type) {
@@ -125,36 +165,56 @@ var Factory = {
     }
 
     return Factory.single(options, 'input');
-  }, label: function(options) {
+  },
+
+  label: function(options) {
     return Factory.double(options, 'label');
-  }, legend: function(options) {
+  },
+
+  legend: function(options) {
     return Factory.double(options, 'legend');
-  }, option: function(options) {
+  },
+
+  option: function(options) {
     return Factory.double(options, 'option');
-  }, password: function(options) {
+  },
+
+  password: function(options) {
     Factory._verify(options);
 
     return Factory.input(options, 'password');
-  }, radio: function(options) {
+  },
+
+  radio: function(options) {
     Factory._verify(options);
 
     return Factory.input(options, 'radio');
-  }, select: function(options) {
+  },
+
+  select: function(options) {
     return Factory.double(options, 'select');
-  }, single: function(options, name) {
+  },
+
+  single: function(options, name) {
     var data = Factory._data(options),
-        tag  = '<' + name + ' ' + data.attributes + ' />';
+      tag  = '<' + name + ' ' + data.attributes + ' />';
 
     return Factory._repeat(tag.replace('  />', ' />'), data);
-  }, submit: function(options) {
+  },
+
+  submit: function(options) {
     Factory._verify(options);
 
     return Factory.input(options, 'submit');
-  }, text: function(options) {
+  },
+
+  text: function(options) {
     Factory._verify(options);
 
     return Factory.input(options, 'text');
-  }, textarea: function(options) {
+  },
+
+  textarea: function(options) {
     return Factory.double(options, 'textarea');
   }
 };
